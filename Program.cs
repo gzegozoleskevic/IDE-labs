a6c3cad6aa23289c4ecd61f07136b00713cf8c51
gzegozoleskevic <64555054+gzegozoleskevic@users.noreply.github.com>
2020-04-29 22:25:17 +0300

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,25 @@
+/*
+ * Created by SharpDevelop.
+ * User: Grzes
+ * Date: 4/3/2020
+ * Time: 9:50 PM
+ * 
+ * To change this template use Tools | Options | Coding | Edit Standard Headers.
+ */
+using System;
+
+namespace a
+{
+	class Program
+	{
+		public static void Main(string[] args)
+		{
+			Console.WriteLine("Hello World!");
+			
+			// TODO: Implement Functionality Here
+			
+			Console.Write("Press any key to continue . . . ");
+			Console.ReadKey(true);
+		}
+	}
+}
